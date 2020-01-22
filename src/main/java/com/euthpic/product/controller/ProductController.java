875c05ef130933b5d81dee4c4c2ed6f7846a92a5
Euthpic <490114771@qq.com>
2020-01-22 20:28:46 +0800

    完善配置文件
    Signed-off-by: Euthpic <490114771@qq.com>

---
+++ src/main/java/com/euthpic/product/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.euthpic.product.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+    /**
+     * 1. 查询所有上架商品
+     * 2. 获取类目type列表
+     * 3. 查询类目
+     * 4. 构造数据
+     */
+    public void list(){
+
+    }
+}
